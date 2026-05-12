5d852363ee3118969c019d8e698189f13e3cbb97
drjstancer <drj.stancer@gmail.com>
2026-05-11 20:09:03 -0500

    Complete ecosystem unification with shared transitions and mobile navigation

--- components/SiteLayout.jsx
+++ components/SiteLayout.jsx
@@ -1,14 +1,24 @@
 import Navigation from './Navigation'
+import MobileNavigation from './MobileNavigation'
 import Footer from './Footer'
+import PageTransition from './PageTransition'
 
 export default function SiteLayout({ children }) {
   return (
     <div className="bg-[#070707] min-h-screen text-[#F5F2EB] overflow-x-hidden">
-      <Navigation />
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <Navigation />
 
-      <div className="pt-24">
-        {children}
+        <div className="absolute top-6 right-6 lg:hidden z-50">
+          <MobileNavigation />
+        </div>
       </div>
+
+      <PageTransition>
+        <div className="pt-24 min-h-screen">
+          {children}
+        </div>
+      </PageTransition>
 
       <Footer />
     </div>
