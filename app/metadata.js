a523cc87a8e29c7cb289dce83d76c8adcbb74d4e
drjstancer <drj.stancer@gmail.com>
2026-05-11 19:09:47 -0500

    Add metadata configuration

---
+++ app/metadata.js
@@ -0,0 +1,7 @@
+export const siteMetadata = {
+  title: 'get ACEd, LLC | Dr. Joel Stancer',
+  description:
+    'Transformational educational consulting rooted in leadership, mentorship, culture, and community-centered excellence.',
+  url: 'https://www.getacedllc.com',
+  ogImage: '/hero-drj.png',
+}
